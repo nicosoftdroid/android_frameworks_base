f7bfec1a7b1b97ed757b8cc5ad07c4d5c172fe62
zigackly <zigackerly@gmail.com>
2012-10-08 10:24:24 +0100

    Expanded desktop capability (2/2)
            Originally authored by Jesús David and ported from
            ParanoidAndroid ICS branch
            https://github.com/ParanoidAndroid/android_frameworks_base/commit/3b6ee6e7ee0a124eec5841763e05eb57c82422e6
    
            Adds an option to settings to add an expanded desktop option to the power menu.
            By default the option does not show in the power menu.  When used it hides the status bar and
            navigation bar (if there is any).
    
            With thanks to Paul Beeler for much rework.
    
            Patch set 1: Initial revision.
    
            Patch set 2: Replace tabs with spaces in commit message.
    
            Patch set 3: Add history to commit message.
    
    Change-Id: I45b37e24246c7a1ffcdd33019abfcb4ede19805d

--- policy/src/com/android/internal/policy/impl/GlobalActions.java
+++ policy/src/com/android/internal/policy/impl/GlobalActions.java
@@ -348,7 +348,7 @@ class GlobalActions implements DialogInterface.OnDismissListener, DialogInterfac
         }
 
         // next: expanded desktop toggle
-        // only shown if enabled, enabled by default
+        // only shown if enabled, disabled by default
         if(Settings.System.getInt(mContext.getContentResolver(),
                 Settings.System.POWER_MENU_EXPANDED_DESKTOP_ENABLED, 0) == 1){
             mItems.add(mExpandDesktopModeOn);
